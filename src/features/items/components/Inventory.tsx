5e6f138dde3fa2dcb43e8bad72ce6e6bcdb62086
nconnel4 <nconnel4@hotmail.com>
2023-12-06 19:40:18 -0500

    feat: add masks to inventory

--- src/features/items/components/Inventory.tsx
+++ src/features/items/components/Inventory.tsx
@@ -1,9 +1,11 @@
 import { Items } from "./Items";
+import { Masks } from "./Masks.tsx";
 
 export const Inventory = () => {
   return (
     <div className="inventory">
       <Items />
+      <Masks />
     </div>
   );
 };
---
+++ src/features/items/components/Masks.tsx
@@ -0,0 +1,32 @@
+import { Item } from "./Item";
+
+export const Masks = () => {
+  return (
+    <div className={"masks"}>
+      <Item id={"maskPostmanHat"} />
+      <Item id={"maskAllNight"} />
+      <Item id={"maskBlast"} />
+      <Item id={"maskStone"} />
+      <Item id={"maskGreatFairy"} />
+      <Item id={"maskDeku"} />
+      <Item id={"maskKeaton"} />
+      <Item id={"maskBremen"} />
+      <Item id={"maskBunnyHood"} />
+      <Item id={"maskDonGero"} />
+      <Item id={"maskScents"} />
+      <Item id={"maskGoron"} />
+      <Item id={"maskRomani"} />
+      <Item id={"maskCircusLeader"} />
+      <Item id={"maskKafei"} />
+      <Item id={"maskCouples"} />
+      <Item id={"maskTruth"} />
+      <Item id={"maskZora"} />
+      <Item id={"maskKamaro"} />
+      <Item id={"maskGibdo"} />
+      <Item id={"maskGaro"} />
+      <Item id={"maskCaptainHat"} />
+      <Item id={"maskGiant"} />
+      <Item id={"maskFierceDeity"} />
+    </div>
+  );
+};
